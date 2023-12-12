ad3037d0c6048d15a7f8a2d9686bdb8cc038faa5
Tomáš Malčánek <tomas.malcanek@memos.cz>
2023-12-12 19:18:19 +0100

    Init

---
+++ Aresak.Interfacify.Generator/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Aresak.Interfacify.Generator
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Aresak.Interfacify.Samples/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Aresak.Interfacify.Samples;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
